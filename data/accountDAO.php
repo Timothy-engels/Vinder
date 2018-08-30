23882880fc6320d047307b39d70b18d7ea4d90e3
kambeb <kamil@bebenek.net>
2018-08-30 12:14:37 +0200

    new parameters in the class

--- data/accountDAO.php
+++ data/accountDAO.php
@@ -2,18 +2,20 @@
 //data/accountDAO.php
 
 require_once("DBConfig.php");
-require_once("entities/account.php");
+require_once("../entities/account.php");
 
-class CommentDAO
+
+
+class AccountDAO
 {
     public function getAll()
     {
-        $sql = "select naam as name, contactpersoon as contactPerson, emailadres as email, wachtwoord as password, bevestigd as confirmed, admin as adniminstrator from accounts";
+        $sql = "select naam as name, contactpersoon as contactPerson, emailadres as email, wachtwoord as password, bevestigd as confirmed, logo, website, info from accounts, admin as adniminstrator";
         $dbh = new PDO(DBConfig::$DB_CONNSTRING, DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
         $resultSet = $dbh->query($sql);
         $list = array();
         foreach ($resultSet as $row) {
-            $account = Account::create($row["name"], $row["contactPerson"], $row["email"], $row["password"], $row["confirmed"], $row["password"]);
+            $account = Account::create($row["name"], $row["contactPerson"], $row["email"], $row["password"], $row["confirmed"], $row["logo"], $row["website"], $row["info"], $row["administrator"]);
             array_push($list, $account);
         }
         $dbh = null;
