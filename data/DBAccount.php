900334b2ced0a14069225e53f6a4e76e87f95f84
Timothy Engels <timothy.engels@vdabheverlee.intra>
2018-08-30 12:22:12 +0200

    Profiel pagina aanpasses

--- data/DBAccount.php
+++ data/DBAccount.php
@@ -5,7 +5,17 @@ require_once("entities/account.php");
     
 class GebruikerDAO{
     
-    protected function GetAll {
-        $sql ="select ] from accounts"
+    protected function GetProfiel {
+        $sql ="select ID, contactpersoon, Emailadres, info, Logo, naam, website  from accounts";
+        $dbh = new PDO(DBConfig::$DB_CONNSTRING,DBConfig::$DB_USERNAME, DBConfig::$DB_PASSWORD);
+        $resultset = $dbh->querry($sql);
+        
+        $lijst = array();
+        foreach($resultset as $rij){
+            $profiel = Account::create($rij["ID"],$rij["contactpersoon"],$rij["Emailadres"],$rij["info"],$rij["Logo"],$rij["naam"],$rij["website"]);
+            array_push($lijst, $profiel)
+        }
+        $dbh = NULL;
+        return $lijst;
     }
 }
--- data/DBConfig.php
+++ data/DBConfig.php
@@ -4,6 +4,6 @@
 class DBConfig {
     
     public static $DB_CONNSTRING ="mysql:host=localhost;dbname=vinder;charset=utf8";
-    public static $DB_USERNAME = "cursusgebruiker";
-    public static $DB_PASSWORD = "cursuspwd";
+    public static $DB_USERNAME = "atos_vdab";
+    public static $DB_PASSWORD = "atos";
 }
