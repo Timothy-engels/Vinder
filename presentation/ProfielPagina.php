417972f52e8ee734c81851b11057f6fd1696c1c4
Titmohy Engels <timothy.engels.1@gmail.com>
2018-09-01 16:27:29 +0200

    basis layout

--- presentation/ProfielPagina.php
+++ presentation/ProfielPagina.php
@@ -9,7 +9,21 @@
         <h1> Hoofding </h1>
 
         <form action="#" method="post">
+            <img src="#" alt="Logo">
+            <p>jouw logo</p>
+            <input type="submit" name="upload" value="upload">
+            <br>
+            meer informatie : <input type="text" name="Info" placeholder="<?php print($accounts->getInfo());?>">
+            <br>
             naam : <input type="text" name="naam" placeholder="<?php print($accounts->getName()); ?>" />
+            <br>
+            contactpersooon : <input type="text" name="contactpersoon" placeholder="<?php print($accounts->getContactperson()); ?>">
+            <br>
+            e-mail : <input type="email" name="email" placeholder="<?php print($accounts->getemail()); ?>">
+            <br>
+            website : <input type="url" name="website" placeholder="<?php print($accounts->getwebsite()); ?>">
+            <br>
+            
         </form>
     </body>
 </html>
