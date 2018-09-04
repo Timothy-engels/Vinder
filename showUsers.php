bb539d04dbeb67081202b964d698a2f4e482e935
Timothy Engels <timothy.engels.1@gmail.com>
2018-09-04 08:36:40 +0200

    showUsers was nog niet geupdate op de pc in het opleiding centrum

--- showUsers.php
+++ showUsers.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once("business/accountService.php");
-include("presentation/accountList.php");
 
 $usersSvc = new AccountService();
 $accounts = $usersSvc->getAccounts();
+include("presentation/accountList.php");
