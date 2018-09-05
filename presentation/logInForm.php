6ed761ab9391297477d84cd084151f23dea006cc
Van Aerschot Bart <vanaerschotb@hotmail.com>
2018-09-05 16:02:14 +0200

    login procedure

---
+++ presentation/logInForm.php
@@ -0,0 +1,32 @@
+<?php
+    
+    session_start();
+        
+    if (isset($_SESSION["ID"])) {
+        header("location: end.php");
+    }
+	
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset=utf-8>
+	<title>Inloggen</title>
+	<style>
+        body {
+            margin: 0px auto;
+			text-align: center;
+        }
+	</style>
+    </head> 
+    <body>
+		<h1>Inloggen</h1>
+		<form action="../business/logIn.php" method="POST">
+            <p><label for="mail">E-mail: </label><input type="email" name="mail" required></p>
+            <p><label for="pass">Wachtwoord: </label><input type="password" name="pass" required></p>
+            <input type="hidden" name ="process" value="1">
+            <p><input type="submit" value="Inloggen"></p>
+		</form>
+		<a href="resetPassword.php">Wachtwoord vergeten</a><br><a href="">Account aanmaken</a>
+    </body>
+</html>
