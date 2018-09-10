86a99a93b4415936642e280913116f356a6a08b9
Van Aerschot Bart <vanaerschotb@hotmail.com>
2018-09-10 15:53:44 +0200

    een hidden input nagekeken en besloten dat die zen beste tijd heeft gehad, verwijderd

--- presentation/logInForm.php
+++ presentation/logInForm.php
@@ -15,9 +15,8 @@
 		<form action="" method="POST">
             <p><label for="mail">E-mail: </label><input type="email" name="mail" required></p>
             <p><label for="pass">Wachtwoord: </label><input type="password" name="pass" required></p>
-            <input type="hidden" name ="process" value="1">
             <p><input type="submit" value="Inloggen"></p>
 		</form>
-		<a href="resetPassword.php">Wachtwoord vergeten</a><br><a href="register.php">Account aanmaken</a>
+		<a href="business/resetPassword.php">Wachtwoord vergeten</a><br><a href="register.php">Account aanmaken</a>
     </body>
 </html>
