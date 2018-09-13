0047a657f3f144cd85c68da9cd9806a0381df8a7
kambeb <kamil@bebenek.net>
2018-09-13 16:03:29 +0200

    profiel wijzigen aanpassen

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,38 @@
 window.onload = function () {
     console.log(expertises);
 
-    var exps = document.getElementsByClassName('expertise');
-    for (var i = 0; i < exps.length; i++ ){
-      console.log(exps[i]);
-    }
+    moreInfo("expertise");
+    moreInfo("expectedExpertise");
 
 
 };
+
+function moreInfo(expertise) {
+    var exps = document.getElementsByClassName(expertise);
+    for (var i = 0; i < exps.length; i++ ){
+        exps[i].addEventListener('click',function (e) {
+            var id = this.getAttribute('id');
+            if(this.checked){
+                var inputExtra = document.createElement("input");
+                var inputLabel = document.createElement("label");
+
+                inputExtra.setAttribute("id","input"+id);
+                inputExtra.setAttribute("name","input"+id);
+                inputExtra.setAttribute("type","text");
+                inputExtra.setAttribute("class","row");
+
+                inputLabel.setAttribute("id","inputlabel"+id);
+                inputLabel.innerHTML = "More info: ";
+                inputLabel.setAttribute("class","row");
+
+
+                this.parentNode.insertBefore(inputExtra, this.nextSibling.nextSibling.nextSibling);
+                this.parentNode.insertBefore(inputLabel, this.nextSibling.nextSibling.nextSibling);
+            }else{
+                $('#input'+id).remove();
+                $('#inputlabel'+id).remove();
+            }
+
+        })
+    }
+}
--- presentation/accountEdit.php
+++ presentation/accountEdit.php
@@ -4,6 +4,7 @@
         <meta charset="utf-8" />
         <title>Profiel Pagina</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
         <script>
             var expertises = [];
@@ -17,40 +18,52 @@
 
         <?php include('menu.php'); ?>
 
-        <h1> Profiel wijzigen </h1>
+
         
-        <div class="container">
+        <div class="container" >
+            <h1> Profiel wijzigen </h1>
             <input action="aanpassenProfiel.php" method="post" type="hidden">
                 <img src="images/<?php echo $account->getLogo(); ?>" alt="Logo" style="max-width: 150px">
                 <span class="btn btn-file" > Browse <input type="file"></span>
-                
-            <div class="row"><span class="col">naam :</span> <span class="col"><input type="text" name="naam" placeholder="" value = "<?php echo $account->getName(); ?>"/></span></div>
+            <div class="container" >
+            <div class="row"><span class="col-4">naam :</span> <input class="col-8" type="text" name="naam" placeholder="" value = "<?php echo $account->getName(); ?>"/></div>
 
-            <div class="row"><span class="col">meer informatie :</span> <span class="col"><input type="text" name="Info" placeholder="" value = "<?php echo $account->getInfo(); ?>"></span></div>
+            <div class="row"><span class="col-4">meer informatie :</span> <input class="col-8" type="text" name="Info" placeholder="" value = "<?php echo $account->getInfo(); ?>"></div>
 
-            <div class="row"><span class="col">website :</span> <span class="col"><input type="url" name="website" placeholder="" value = "<?php echo $account->getWebsite(); ?>"></span></div>
-
-
-            <h3>Mijn expertise:</h3>
-            <div>
-                <?php
-                foreach ($allExps as $expertise){ ?>
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="expertise custom-control-input" id="expertise<?php echo $expertise->getId(); ?>">
-                        <label class="custom-control-label" for="expertise<?php echo $expertise->getId(); ?>"><?php echo $expertise->getExpertise(); ?></label>
-                    </div>
-                <?php } ?>
+            <div class="row"><span class="col-4">website :</span> <input class="col-8" type="url" name="website" placeholder="" value = "<?php echo $account->getWebsite(); ?>"></div>
             </div>
 
-            <h3>Waarover wil ik informatie?:</h3>
-            <div>
-                <?php
-                foreach ($allExps as $expertise){ ?>
-                    <div class="custom-control custom-checkbox">
-                        <input type="checkbox" class="expectedExpertise custom-control-input" id="expected<?php echo $expertise->getId(); ?>">
-                        <label class="custom-control-label" for="expected<?= $expertise->getId(); ?>"><?php echo $expertise->getExpertise(); ?></label>
+
+            <div class="container">
+                <h3>Mijn expertise:</h3>
+                    <div class="row">
+                        <?php
+                        foreach ($allExps as $expertise){ ?>
+                            <div class="custom-control custom-checkbox container">
+                                <input type="checkbox" class="row expertise custom-control-input" id="expertise<?php echo $expertise->getId(); ?>" <?php
+                                foreach($exps as $exp){
+                                    if($exp===$expertise) echo "checked";
+                                };
+                                ?>>
+                                <label class="row custom-control-label" for="expertise<?php echo $expertise->getId(); ?>"><?php echo $expertise->getExpertise();?></label>
+                            </div>
+                        <?php } ?>
                     </div>
-                <?php } ?>
+
+                    <h3>Waarover wil ik informatie?:</h3>
+                    <div class="row">
+                        <?php
+                        foreach ($allExps as $expertise){ ?>
+                            <div class="custom-control custom-checkbox container">
+                                <input type="checkbox" class="row expectedExpertise custom-control-input" id="expected<?php echo $expertise->getId(); ?>" <?php
+                                foreach($expExps as $exp){
+                                    if($exp===$expertise) echo "checked";
+                                };
+                                ?>>
+                                <label class="row custom-control-label" for="expected<?= $expertise->getId(); ?>"><?php echo $expertise->getExpertise(); ?></label>
+                            </div>
+                        <?php } ?>
+                    </div>
             </div>
 
 
