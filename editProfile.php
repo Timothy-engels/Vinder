87d6e74b8a3599652b008ee77633f03c82d4dd02
Timothy Engels <timothy.engels.1@gmail.com>
2018-09-19 13:46:03 +0200

    foutoplossing editProfile en showProfile terug gezet

---
+++ editProfile.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once("business/expertiseService.php");
+require_once("business/accountService.php");
+
+
+$accountSvc = new AccountService();
+// Check if user is logged in
+$account    = $accountSvc->getLoggedInUser();
+$info       = $account->getInfo();
+
+
+// Is the user logged in as an admin
+$loggedInAsAdmin = ($account->getAdministrator() === "1" ? true : false);
+
+// Get the ID from the logged in user
+$id = $account->getId();
+
+// Get the necessary info to display the view
+$expSrv      = new ExpertiseService();
+$exps        = $expSrv->getExpertisesById($id);
+$expExps     = $expSrv->getExpectedExpertisesById($id);
+$extraExp    = $expSrv->getExtraExpertise($id);
+$extraExpExp = $expSrv->getExtraExpectedExpertise($id);
+$allExps     = $expSrv->getExpertises();
+
+include("presentation/accountEdit.php");
---
+++ showProfile.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once("business/expertiseService.php");
+require_once("business/accountService.php");
+
+$accountSvc = new AccountService();
+// Check if user is logged in
+$account    = $accountSvc->getLoggedInUser();
+$info       = $account->getInfo();
+
+
+// Is the user logged in as an admin
+$loggedInAsAdmin = ($account->getAdministrator() === "1" ? true : false);
+
+// Get the ID from the logged in user
+$id = $account->getId();
+
+// Get the necessary info to display the view
+$expSrv      = new ExpertiseService();
+$exps        = $expSrv->getExpertisesById($id);
+$expExps     = $expSrv->getExpectedExpertisesById($id);
+$extraExp    = $expSrv->getExtraExpertise($id);
+$extraExpExp = $expSrv->getExtraExpectedExpertise($id);
+
+include("presentation/accountShow.php");
