060b91520c0cffe540b00f361b22b4ef935dcce7
Timothy Engels <timothy.engels.1@gmail.com>
2018-09-19 13:37:47 +0200

    editProfile en showProfile samengevoegd in 1 file en getest

--- dashboard.php
+++ dashboard.php
@@ -5,7 +5,7 @@ require_once("business/accountService.php");
 
 $accountSvc = new AccountService();
 // Check if user is logged in
-$account    = $accountSvc->getLoggedInUser(true);
+$account    = $accountSvc->getLoggedInUser();
 $info       = $account->getInfo();
 
 
