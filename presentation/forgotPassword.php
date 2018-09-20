e1a241137d0e3151f4e2c1b1925415b71b25051a
Cindy Clijsters <cindy.clijsters@gmail.com>
2018-09-20 10:59:29 +0200

    Added focus on first field by forgotten a password

--- presentation/forgotPassword.php
+++ presentation/forgotPassword.php
@@ -18,7 +18,7 @@
         <p>Gelieve uw e-mailadres in te geven.<br>We sturen je een e-mail waarmee je je wachtwoord kan resetten.</p>
         <form action="forgotPassword.php" method="POST">
             <label for="mail">E-mail : </label>
-            <input type="text" name="mail">
+            <input type="text" name="mail" autofocus>
             <?php if (array_key_exists('mail', $errors)) : ?>
                 <div class="error"><?= $errors['mail']; ?></div>
             <?php endif; ?>
