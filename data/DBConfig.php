8f9c7c5cd7c367a5c0df146357cee04fdd128cc6
kambeb <kamil@bebenek.net>
2018-09-26 14:35:07 +0200

    Merge branch 'master' of https://github.com/Timothy-engels/Vinder
    # Conflicts:
    #	data/DBConfig.php
    #	editProfile.php
    #	entities/expertise.php
    #	presentation/accountEdit.php

--- data/DBConfig.php
+++ data/DBConfig.php
@@ -2,16 +2,16 @@
 //data/DBConfig.php
 
 class DBConfig {
+    public static $DB_CONNSTRING = "mysql:host=sql.s22.vdl.pl;dbname=atos_vdab;charset=utf8";
+    public static $DB_USERNAME = "atos_vdab";
+    public static $DB_PASSWORD = "vdab";
+}
+
+
+
+/*class DBConfig {
 
     public static $DB_CONNSTRING ="mysql:host=localhost;dbname=vinder;charset=utf8";
     public static $DB_USERNAME = "cursusgebruiker";
     public static $DB_PASSWORD = "cursuspwd";
-}
-
-
-/*class DBConfig
-{
-    public static $DB_CONNSTRING = "mysql:host=sql.s22.vdl.pl;dbname=atos_vdab;charset=utf8";
-    public static $DB_USERNAME = "atos_vdab";
-    public static $DB_PASSWORD = "vdab";
 }*/
