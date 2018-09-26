9985a9b6b5cf55dca8172eb9a01762553ab9b717
fritstoofvlees <vanaerschotb@hotmail.com>
2018-09-26 21:16:06 +0200

    paar kleine aanpassingen op presentation/swipeCard.php gemaakt, niets revolutionair, had ff pauze van AJAX nodig

--- presentation/swipeCard.php
+++ presentation/swipeCard.php
@@ -7,19 +7,20 @@
             body {
                 margin: 0px auto;
                 text-align: center;
+                background-color: blue;
             }
             .swipingArea {
                 
             }
             #swipeCard { 
                 width: 350px;
-                height: 200px; 
-                background-color: red;
+                height: 512px; 
+                background-color: white;
                 display: inline-block;
             }
             #no, #yes {
-                width: 400px;
-                height: 250px;
+                width: 512px;
+                height: 512px;
                 background-color: blue;
                 display: inline-block;
             }
@@ -27,23 +28,26 @@
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
         <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js"></script>
         <script>
-            if (window.XMLHttpRequest) {
-                // code for modern browsers
-                xmlhttp = new XMLHttpRequest();
-            } else {
-                // code for old IE browsers
-                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-            }
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("swipeCard").innerHTML =
-                        this.$json;
+            $(document).ready( function() {
+                if (window.XMLHttpRequest) {
+                    // code for modern browsers
+                    xmlhttp = new XMLHttpRequest();
+                } else {
+                    // code for old IE browsers
+                    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
                 }
-            };
-            xhttp.open("GET", "swipe.php", true);
-            xhttp.send();
-            
-            $(document).ready( function() { 
+                /*       hoofdpijngenerator
+                
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        document.getElementById("swipeCard").innerHTML =
+                            this.responseText;
+                    }
+                };
+                xhttp.open("GET", "swipe.php", true);
+                xhttp.send();
+                */
+             
                 $("#swipeCard").html();
                 $("#swipeCard").draggable();
                 $("#no").droppable( { drop: function( event, ui ) {
@@ -57,11 +61,11 @@
     </head> 
     <body>
         <div class="swipingArea">
-            <div id="no">
+            <div id="no" style="background-image: url('images/swipe_left.png');">
             </div>
             <div id="swipeCard">
             </div>
-            <div id="yes">
+            <div id="yes" style="background-image: url('images/swipe_right.png');">
             </div>
         </div>
     </body>
