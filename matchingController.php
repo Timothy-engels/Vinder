c9876ad8fb042300bc4d5be4fb00399f4c7135f5
Cindy Clijsters <cindy.clijsters@gmail.com>
2018-09-27 11:02:10 +0200

    Removed double code

--- matchingController.php
+++ matchingController.php
@@ -1,9 +1,15 @@
 <?php
 // matchingController.php
 
-require_once ('business/matchingService.php');
+require_once('business/accountService.php');
 
-$mS = new matchingService() ;
-$mO = $mS->getMatchOverzicht();
+// Load the account service
+$accountSvc = new AccountService();
+
+// Get the information of the logged in user
+$account = $accountSvc->getLoggedInUser();
+
+// Get an array with the matched companies
+$mO = $accountSvc->getMatchedCompanies($account->getID());
 
 include ('presentation/matches.php');
