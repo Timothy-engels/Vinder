5b645cfcf6c636a37adbb8794f3c1f22f9d3ef85
Cindy Clijsters <cindy.clijsters@vdabheverlee.intra>
2018-09-28 12:09:06 +0200

    Remove all matchings from database

---
+++ presentation/deleteMatchingConfirmation.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset=utf-8>
+    <title>Vinder | Matchings verwijderen</title>
+</head>
+<body>
+
+    <?php include('menu.php'); ?>
+    
+    <main>
+            
+        <h1>Matchings verwijderen</h1>
+
+        <p><?= $result; ?></p>
+        
+    </main>
+
+</body>
+</html>
