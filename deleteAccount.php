33f492e9a7e68cf9a0f63fa1d5fbc7d6c94b5821
Van Aerschot Bart <vanaerschotb@hotmail.com>
2018-10-01 15:57:00 +0200

    foutmeldingen bij verwijderen account (door gebruiker en admin) met tipp-ex overschreven en paar kleine aanpassingen gemaakt

--- deleteAccount.php
+++ deleteAccount.php
@@ -3,18 +3,26 @@ require_once("business/expertiseService.php");
 require_once("business/accountService.php");
 require_once('business/matchingService.php');
 
-$usersSvc = new AccountService();
+$accountSvc = new AccountService();
 $expSrv      = new ExpertiseService();
 $matchSrv      = new MatchingService();
 
 // Check if user is logged in
-$account = $usersSvc->getLoggedInUser();
+$account = $accountSvc->getLoggedInUser();
 
 // Is the user logged in as an admin
 $loggedInAsAdmin = ($account->getAdministrator() === "1" ? true : false);
 
 // Get the ID from the logged in user
 $id = $account->getId();
+
+if(!isset($_POST['pass'])) {
+    $_POST['pass'] = "";
+}
+
+if(!isset($_POST['del'])) {
+    $_POST['del'] = "";
+}
 
 if (($_SERVER['REQUEST_METHOD'] === 'POST') and $_POST['pass']) {
     if (password_verify($_POST['pass'],$account->getPassword())) {
@@ -46,7 +54,7 @@ if (($_SERVER['REQUEST_METHOD'] === 'POST') and $_POST['pass']) {
     }
 }elseif(($_GET["id"]) and $loggedInAsAdmin ){
     $id = ($_GET["id"]);
-    $account = $usersSvc->getById($id);
+    $account = $accountSvc->getById($id);
     if ($account){
         if($_POST['del'] === $id){
 
@@ -63,9 +71,10 @@ if (($_SERVER['REQUEST_METHOD'] === 'POST') and $_POST['pass']) {
             @$oldlogo = $account->getLogo($newfilename);
             @unlink('images/' . $oldlogo);//logo from server remove
 
-            $usersSvc->deleteById($id);//remove account
+            $accountSvc->deleteById($id);//remove account
 
-            echo "Account id: ".$id." has been deleted";
+            echo "Account id: ".$id." is verwijderd.<br>
+                  <a href='userList.php'>Terug naar lijst</a>";
         }else include("presentation/accountDeleteAdmin.php");
 
     }
