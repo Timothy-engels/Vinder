e561ce9a901d22e6b997fe17ff8f85426430d293
Van Aerschot Bart <vanaerschotb@hotmail.com>
2018-10-03 11:32:44 +0200

    lay-out toegevoegd aan pagina presentation/forgotPassword

--- presentation/forgotPassword.php
+++ presentation/forgotPassword.php
@@ -1,29 +1,71 @@
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <meta charset=utf-8>
-	<title>Vinder | Wachtwoord vergeten?</title>
-	<style>
-            label, input, .error {
-                display: block;
-            }
-            
-            .error {
-                color: red;
-            }
-	</style>
-    </head> 
-    <body>
-        <h1>Wachtwoord vergeten?</h1>
-        <p>Gelieve uw e-mailadres in te geven.<br>We sturen je een e-mail waarmee je je wachtwoord kan resetten.</p>
-        <form action="forgotPassword.php" method="POST">
-            <label for="mail">E-mail : </label>
-            <input type="text" name="mail" autofocus>
-            <?php if (array_key_exists('mail', $errors)) : ?>
-                <div class="error"><?= $errors['mail']; ?></div>
-            <?php endif; ?>
-                        
-            <input type="submit" value="Verzenden">
-        </form>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" name="viewport">
+    <title>Wachtwoord vergeten</title>
+    <link rel="stylesheet" href="modules/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="modules/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="modules/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="css/style.css">    
+    <link rel="stylesheet" href="css/skins/vinder.css"> 
+</head>
+
+<body>
+  <div id="app">
+    <section class="section">
+      <div class="container mt-5">
+        <div class="row">
+          <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+            <div class="login-brand">
+                <a href="logIn.php"><img src="images/logo.png" alt="Vinder" style="width: 15rem;"></a>
+            </div>
+
+            <div class="card card-primary">
+              <div class="card-header"><h4>Wachtwoord vergeten</h4></div>
+
+              <div class="card-body">
+                <p class="text-muted">
+                    We sturen u een link via mail om uw wachtwoord te resetten. 
+                </p>
+                <form method="POST" action="forgotPassword.php">
+                  <div class="form-group">
+                    <label for="email">Email</label>
+                    <input id="mail" type="email" class="form-control" name="mail" tabindex="1" required autofocus>
+                  </div>
+                    
+                    <?php if (array_key_exists('mail', $errors)) : ?>
+                        <div class="error"><?= $errors['mail']; ?></div>
+                    <?php endif; ?>
+
+                  <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-block" tabindex="4">
+                      Verzend
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div class="simple-footer">
+              Copyright &copy; VDAB 2018
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <script src="../dist/modules/jquery.min.js"></script>
+  <script src="../dist/modules/popper.js"></script>
+  <script src="../dist/modules/tooltip.js"></script>
+  <script src="../dist/modules/bootstrap/js/bootstrap.min.js"></script>
+  <script src="../dist/modules/nicescroll/jquery.nicescroll.min.js"></script>
+  <script src="../dist/modules/moment.min.js"></script>
+  <script src="../dist/modules/scroll-up-bar/dist/scroll-up-bar.min.js"></script>
+  <script src="../dist/js/sa-functions.js"></script>
+  
+  <script src="../dist/js/scripts.js"></script>
+  <script src="../dist/js/custom.js"></script>
+  <script src="../dist/js/demo.js"></script>
+</body>
 </html>
