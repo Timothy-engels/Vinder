992fc27a85d6f1e91d9106eed128964faa48a868
fritstoofvlees <vanaerschotb@hotmail.com>
2018-10-04 00:15:11 +0200

    lay-out aan presentation/deleteMatchingSucces toegevoegd

--- presentation/deleteMatchingSuccess.php
+++ presentation/deleteMatchingSuccess.php
@@ -1,20 +1,43 @@
 <!DOCTYPE html>
 <html lang="nl">
-    <head>
-        <meta charset="UTF-8">
-        <title>Vinder | Matchings verwijderen?</title>
-    </head>
-    <body>
-        <?php include('menu.php'); ?>
+<head>
+    <meta charset="UTF-8">
+    <title>Vinder | Matchings verwijderen?</title>
+    <link rel="stylesheet" href="modules/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="modules/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="modules/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="css/style.css">    
+    <link rel="stylesheet" href="css/skins/vinder.css">
+</head>
+<body>
+    
+    <?php include('menu.php'); ?>
         
-        <main>
-                
-            <h1>Matchings verwijderen?</h1>
-
-            <p>Er is een bevestigingsmail verstuurd naar je e-mail adres.</p>
-
-            <p>Let op: de link in deze mail is slechts één uur geldig!</p>
-
-        </main>
-    </body>
+    <div id="app">
+        <section class="section">
+            <div class="container mt-5">
+                <div class="row">
+                    <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                        <div class="login-brand">
+                            <a href="logIn.php"><img src="images/logo.png" alt="Vinder" style="width: 15rem;"></a>
+                        </div>
+                        <div class="card card-primary">
+                            <div class="card-header">
+                                <h4>Matchings verwijderen?</h4>
+                            </div>
+                            <div class="card-body">
+                                <p class="text-muted">
+                                    Er is een bevestigingsmail verstuurd naar je e-mail adres. Let op: de link in deze mail is slechts één uur geldig! 
+                                </p>
+                            </div>
+                        </div>
+                        <div class="simple-footer">
+                            Copyright &copy; VDAB 2018
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>        
+</body>
 </html>
