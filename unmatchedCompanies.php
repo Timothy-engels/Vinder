4a6ad8241b40cb0499814691e5ce0948a2ec1088
Cindy Clijsters <cindy.clijsters@vdabheverlee.intra>
2018-10-03 09:36:23 +0200

    Cleaned up some code

--- unmatchedCompanies.php
+++ unmatchedCompanies.php
@@ -12,30 +12,33 @@ $loggedInAsAdmin = ($account->getAdministrator() === "1" ? true : false);
 // Get al ist with the unmachted companies
 $unmatchedCompanies = $accountSvc->getUnmatchedCompanies();
 
-// match with admin/VDAB
-$matchSvc = new matchingService();
+if (isset ($_POST["VDAB"]) && $_POST["VDAB"] == "match met VDAB") {
 
-if (isset ($_POST["VDAB"]) && $_POST["VDAB"] == "match met VDAB"){
-
+    // Get ID from admin company
     $idAdmin = $account->getId();
-    $status = 3;
-    foreach ( $unmatchedCompanies as $uC){
+    
+    // Set the status
+    $status  = 3;
+    
+    // Match with admin
+    $matchSvc = new matchingService();
+
+    foreach ($unmatchedCompanies as $uC) {
         
-        // retrieving ID
+        // Get ID from second company company
         $idUnmatchedCompany = $uC->getId();
         
-        // insert into DB
+        // Insert into DB
         $MatchWithAdmin = $matchSvc->insert($idAdmin, $idUnmatchedCompany, $status);
         
-        // sent matching mail
-        $mailTo = $accountSvc->sendMatchFoundMails($idAdmin, $idUnmatchedCompany);
+        // Sent matching mail
+        $accountSvc->sendMatchFoundMails($idAdmin, $idUnmatchedCompany);
             
     }   
-    // Get al ist with the unmachted companies
+    
+    // Get a list with the unmatched companies
     $unmatchedCompanies = $accountSvc->getUnmatchedCompanies();
 }
-
-
 
 // Show the view
 include 'presentation/unmatchedCompanies.php';
