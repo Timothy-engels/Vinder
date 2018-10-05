7014a59f8e67d1ac26650a4deafe01cd3141c355
fritstoofvlees <vanaerschotb@hotmail.com>
2018-10-05 13:40:18 +0200

    lay-out presentation/updatePasswordSucces aangepast

--- presentation/updatePasswordSuccess.php
+++ presentation/updatePasswordSuccess.php
@@ -3,25 +3,45 @@
     <head>
         <meta charset="UTF-8">
         <title>Vinder | Registreer</title>
+        <link rel="stylesheet" href="modules/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="modules/ionicons/css/ionicons.min.css">
+        <link rel="stylesheet" href="modules/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
+        <link rel="stylesheet" href="css/style.css">    
+        <link rel="stylesheet" href="css/skins/vinder.css">
     </head>
     <body>
-        <?php
-        if ($showMenu) {
-            include('menu.php');
-        }
-        ?>
         
-        <main>
-            <section id="register">
-                
-                <h1>Wachtwoord wijzigen</h1>
-                
-                <p>Je wachtwoord is met success gewijzigd.</p>
-                <?php if ($code !== '') : ?>
-                <p><a href="logIn.php">Klik hier om je opnieuw in te loggen.</a></p>
-                <?php endif; ?>
-                
-            </section>
-        </main>
+        <div id="app">
+            <div class="main-wrapper">
+                <?php //include('includes/mainHeader.php'); ?>
+                <div class="main-sidebar">
+                    <?php //include('includes/mainSideBar.php'); ?>
+                </div>
+                <div class="main-content">
+                    <section class="section" id="register">
+                        <h1 class="section-header">
+                            <div><a href="dashboard.php"><img src="images/icon.png" alt="Vinder" style="width: 2rem;"></a>&nbsp;&nbsp;Vinder</div>
+                        </h1>
+
+                        <div class="section-body">
+
+                            <div class="row">
+                                <div class="col-12 col-md-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h1>Wachtwoord wijzigen</h1>
+                                        </div>
+                                        <div class="card-body">
+                                            <p>Je wachtwoord is met success gewijzigd!</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+                <?php include('includes/mainFooter.php'); ?>
+            </div>
+        </div>
     </body>
 </html>
