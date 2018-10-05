3b4f3a00ae24ba65de8172086b213b55ce6336ee
Cindy Clijsters <cindy.clijsters@gmail.com>
2018-10-05 09:26:42 +0200

    Changed language in login form

--- presentation/logIn.php
+++ presentation/logIn.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" name="viewport">
