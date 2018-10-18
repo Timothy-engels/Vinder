b7743a6d540e2279a37d9608793f3b9a3016e04d
Cindy Clijsters <cindy.clijsters@gmail.com>
2018-10-18 14:07:09 +0200

    Show accounts in a list

--- presentation/accounts.php
+++ presentation/accounts.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset=utf-8>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no" name="viewport">
-    <title>Vinder | Lijst gebruikers</title>
+    <title>Vinder | Alle accounts</title>
     <link rel="stylesheet" href="modules/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="modules/ionicons/css/ionicons.min.css">
     <link rel="stylesheet" href="modules/fontawesome/web-fonts-with-css/css/fontawesome-all.min.css">
@@ -22,35 +22,62 @@
         <div class="main-content">
             <section class="section">
                 <h1 class="section-header">
-                    <div><a href="dashboard.php"><img src="images/icon.png" alt="Vinder" style="width: 2rem;"></a>&nbsp;&nbsp;Lijst gebruikers</div>
+                    <div><a href="dashboard.php"><img src="images/logo.png" alt="Vinder" class="logo-small"></a></div>
                 </h1>
                 <div class="section-body">
                     <div class="row">
                         <div class="col-12 col-md-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h4>Lijst gebruikers</h4>
+                                    <h4>Alle accounts</h4>
                                 </div>
                                 <div class="card-body">
-                                    <ul class="list-unstyled list-unstyled-border">
-                                        <?php foreach($list as $row) : ?>
-                                            <li class="media">
-                                                <img class="mr-3 rounded-circle" width="50" src="images/<?= $row->getLogo(); ?>" alt="logo">
-                                                <div class="media-body">
-                                                    <div class="float-right">
-                                                        <a href="account-verwijderen.php?id=<?php print($row->getId()); ?>">
-                                                            (Delete)
-                                                        </a>
-                                                    </div>
-                                                    <div class="media-title">
-                                                        <a href="showProfile.php?id=<?=$row->getId(); ?>">
+                                    <div class="table-responsive">
+                                        <table class="table table-striped">
+                                            <thead>
+                                                <tr>
+                                                    <th>Naam</th>
+                                                    <th>Registratie bevestigd</th>
+                                                    <th>Opties</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                            <?php foreach($list as $row) : ?>
+                                                <tr>
+                                                    <td>
+                                                        <?php
+                                                        $logo = "no-image.png";
+                                                        if ($row->getLogo() !== null && $row->getLogo() !== '') :
+                                                            $logo = $row->getLogo();
+                                                        endif;
+                                                        ?>
+                                                        <a href="showProfile.php?id=<?= $row->getId(); ?>">
+                                                            <div class="logo-wrapper-small mr-3">
+                                                                <img class="rounded-circle" src="images/<?= $logo; ?>" alt="logo">
+                                                            </div>
                                                             <?= $row->getName(); ?>
                                                         </a>
-                                                    </div>
-                                                </div>
-                                            </li>
-                                        <?php endforeach; ?>
-                                    </ul>
+                                                    </td>
+                                                    <td>
+                                                        <?php if ($row->getConfirmed() === "1") : ?>
+                                                            <div class='badge badge-success'>Bevestigd</div>
+                                                        <?php else : ?>
+                                                            <div class='badge badge-danger'>Niet bevestigd</div>
+                                                        <?php endif; ?>
+                                                    </td>
+                                                    <td>
+                                                        <a href="showProfile.php?id=<?= $row->getId(); ?>">
+                                                            Bekijken
+                                                        </a> |
+                                                        <a href="account-verwijderen.php?id=<?= $row->getId(); ?>">
+                                                            Verwijderen
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            <?php endforeach; ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
                                 </div>
                             </div>
                         </div>
