1f300e6bf3e854ffe2d9ba760cc967cd459beb1b
Cindy Clijsters <cindy.clijsters@gmail.com>
2018-10-22 09:40:45 +0200

    Fixed errors when showing profile under dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,32 +1,35 @@
 <?php
-
 require_once("business/expertiseService.php");
 require_once("business/accountService.php");
 
-$accountSvc = new AccountService();
 // Check if user is logged in
-$account = $loggedInAccount = $accountSvc->getLoggedInUser();
-$info       = $account->getInfo();
+$accountSvc      = new AccountService();
+$loggedInAccount = $accountSvc->getLoggedInUser();
 
 // Is the user logged in as an admin
-$loggedInAsAdmin = ($account->getAdministrator() === "1" ? true : false);
+if ($loggedInAccount->getAdministrator() === "1") {
+    $amountMatchedCompanies   = $accountSvc->getAmountMatchedCompanies();
+    $amountUnmatchedCompanies = $accountSvc->getAmountUnmatchedCompanies();
+}
 
 // Get the ID from the logged in user
-$id = $account->getId();
+$id = $loggedInAccount->getId();
 
 // Get the necessary info to display the view
+$account     = $loggedInAccount;
 $expSrv      = new ExpertiseService();
 $exps        = $expSrv->getExpertisesById($id);
 $expExps     = $expSrv->getExpectedExpertisesById($id);
 $extraExp    = $expSrv->getExtraExpertise($id);
 $extraExpExp = $expSrv->getExtraExpectedExpertise($id);
 
+// Show the view
+$info = $loggedInAccount->getInfo();
 
 if ($info === null || $info == "") {
-    $allExps     = $expSrv->getExpertises();
+    $allExps = $expSrv->getExpertises();
     include("presentation/accountEdit.php");
-}
-else {
+} else {
     include("presentation/accountShow.php");
 }
     
